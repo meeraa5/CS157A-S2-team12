06dcc4d676f2da502dc65c1614f317513bf7df37
Samantha <baconspam168@gmail.com>
2026-05-01 23:06:15 -0700

    Create Product.java

---
+++ src/main/java/Product.java
@@ -0,0 +1,123 @@
+import java.time.LocalDate;
+public class Product {
+	private int productId;
+	private String productName;
+	private String info;
+	private int price;
+	private String condition;
+	private int quantityAvail;
+	private String productStatus;
+	private LocalDate dateAdded;
+	private String lowStockNotice;
+	private int userId;
+
+	protected Product() {
+	}
+
+	public Product(
+			int productId,
+			String productName,
+			String info, int price,
+			String condition,
+			int quantityAvail,
+			String productStatus,
+			LocalDate dateAdded,
+			String lowStockNotice, int userId) { // i hate when parameters are like this but it's easier to see
+		// product entity to easily (at least i hope so) create and delete products
+		this.productId = productId;
+		this.productName = productName;
+		this.info = info;
+		this.price = price;
+		this.condition = condition;
+		this.quantityAvail = quantityAvail;
+		this.productStatus = productStatus;
+		this.dateAdded = dateAdded;
+		this.lowStockNotice = lowStockNotice;
+		this.userId = userId;
+
+	}
+	
+	// getters and setters to edit products
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public int getQuantityAvail() {
+		return quantityAvail;
+	}
+
+	public void setQuantityAvail(int quantityAvail) {
+		this.quantityAvail = quantityAvail;
+	}
+
+	public String getProductStatus() {
+		return productStatus;
+	}
+
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
+	}
+
+	public LocalDate getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(LocalDate dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	public String getLowStockNotice() {
+		return lowStockNotice;
+	}
+
+	public void setLowStockNotice(String lowStockNotice) {
+		this.lowStockNotice = lowStockNotice;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+}
