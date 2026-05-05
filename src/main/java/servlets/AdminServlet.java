d8d5e76923e407b375509c3ef14a5781713913c8
Samantha <baconspam168@gmail.com>
2026-05-04 23:56:08 -0700

    Update AdminServlet.java
    reimported restockhistorydao

--- src/main/java/servlets/AdminServlet.java
+++ src/main/java/servlets/AdminServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.ProductDao;
+import dao.RestockHistoryDao;
 import dao.UserDao;
 import util.Product;
 import util.RestockHistory;
